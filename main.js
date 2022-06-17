b069cfaea179b348d5fcbd1a21997b2d3d80c533
Ambar2010 <89517719+Ambar2010@users.noreply.github.com>
2022-06-17 18:59:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function bedroom() {
+    window.location ="bedroom.html";
+}
+function tvandac() {
+    window.location ="ac_and_tv.html";
+}
+function desk() {
+    window.location ="desk.html";
+}
+function bottles() {
+    window.location ="bottles.html";
+}
+function fruitbasket() {
+    window.location ="fruit_basket.html";
+}
